300255ea5f55b70b6dfa90467de6f0283f30c655
unknown <sr.rauniyar@gmail.com>
2015-08-17 22:30:06 +0545

    Ajax started and activity theme set

--- js/settings-page.js
+++ js/settings-page.js
@@ -1,25 +1,95 @@
 jQuery(document).ready(function($) {
-	var counter=0;
+	 var root = document.location.hostname;
+	 var pathArray = window.location.pathname.split( '/' );
+	 var secondLevelLocation = pathArray[1];
+	 var base_url = 'http://'+root+'/'+ secondLevelLocation;
+
+	//Start for goal fields
+	var counter_activity=0;
+	var counter_goal=0;
 	$('body').on('click','#goal-add-button', function () {
 		debugger;
-		var name = $("#goal_name").val();
-		var type = $("#goal_type").val();
-		var advance = $("#advance").attr("checked") ? 1 : 0;
-		$("#goal-fields").find('tbody').append('<tr><td><div class="form-group"><input type="text" name="goal_name_set['+counter+']" class="form-control" value="'+name+'" id="goal_name'+counter+'"></div></td><td>'+type+'</td><td><input type="checkbox" name="goal_advanced['+counter+']"'+(advance==1? 'checked ' : '')+'id="advance['+counter+']" value="'+advance+'"></td><td class="btn"><input type="button" value="Remove" class="btn-sm btn-danger btn-remove" id="goal-remove-button['+counter+']"></td>');
-		    
-		    // .append('<td><div class="form-group"><input type="text" name="goal_name_set['+counter+']" class="form-control" value="'+name+'" id="goal_name'+counter+'"></div></td>')
-		    // .append('<td>'+type+'</td>')
-		    // .append('<td><input type="checkbox" name="goal_advanced['+counter+']"'+(advance==1? 'checked ' : '')+'id="advance['+counter+']" value="'+advance+'"></td>')
-		    // .append('<td class="btn"><input type="button" value="Remove" class="btn-sm btn-danger btn " id="goal-remove-button['+counter+']"></td>')
-		    
-		                
-		   
-	counter++;
+		var goal_name = $("#goal_name").val();
+		var goal_type = $("#goal_type").val();
+		var goal_advance = $("#goal_advance").attr("checked") ? 1 : 0;
+
+		//Start removing the fields
+		$("#goal_name").val('');
+		$("#goal_type").val('');
+		$("#goal_advance").attr("checked" ,false);
+		//End removing the fields
+		$("#goal-fields").find('tbody').append('<tr><td><div class="form-group"><input type="text" name="goal_name_set['+counter_goal+']" class="form-control" value="'+goal_name+'" id="goal_name'+counter_goal+'"></div></td><td>'+goal_type+'</td><td><input type="checkbox" name="goal_advanced['+counter_goal+']"'+(goal_advance==1? 'checked ' : '')+'id="advance['+counter_goal+']" value="'+goal_advance+'"></td><td><input type="button" value="Remove" class="btn-sm btn-danger btn-remove" id="goal-remove-button['+counter_goal+']"></td></tr>');
+
+	counter_goal++;
 	});
 
 	$('body').on('click','.btn-remove', function () {
 		debugger;
 		 $(this).closest("tr").remove();
+	});
+	//End for goal fields
+
+	//Start for activity fields
+	$('body').on('click','#activity-add-button', function () {
+		debugger;
+		var activity_name = $("#activity_name").val();
+		var activity_type = $("#activity_type").val();
+		var activity_advance = $("#activity_advance").attr("checked") ? 1 : 0;
+
+		//Start removing the fields
+		$("#activity_name").val('');
+		$("#activity_type").val('');
+		$("#activity_advance").attr("checked" ,false);
+		//End removing the fields
+		$("#activity-fields").find('tbody').append('<tr><td><div class="form-group"><input type="text" name="activity_name_set['+counter_activity+']" class="form-control" value="'+activity_name+'" id="activity_name'+counter_activity+'"></div></td><td>'+activity_type+'</td><td><input type="checkbox" name="activity_advanced['+counter_activity+']"'+(activity_advance==1? 'checked ' : '')+'id="advance['+counter_activity+']" value="'+activity_advance+'"></td><td class="btn"><input type="button" value="Remove" class="btn-sm btn-danger btn-remove" id="activity-remove-button['+counter_activity+']"></td></tr>');
+
+	counter_activity++;
+	});
+
+	$('body').on('click','.btn-remove', function () {
+		 $(this).closest("tr").remove();
+	});
+	//End for goal fields
+
+	// $.fn.serializeObject = function()
+ //    {
+ //        var o = {};
+ //        var a = this.serializeArray();
+ //        $.each(a, function() {
+ //            if (o[this.name] !== undefined) {
+ //                if (!o[this.name].push) {
+ //                    o[this.name] = [o[this.name]];
+ //                }
+ //                o[this.name].push(this.value || '');
+ //            } else {
+ //                o[this.name] = this.value || '';
+ //            }
+ //        });
+ //        return o;
+ //    };
+
+	$('body').on('click','.btn-submit-second', function (event){
+		var datastring = $("#profile_settings_ga").serialize();
+		// var datastring = JSON.stringify($('#profile_settings_ga').serializeObject());
+		// datastring = datastring.concat(
+  //           jQuery('form input[type=checkbox]:not(:checked)').map(
+  //                   function() {
+  //                       return {"name": this.name, "value": this.value}
+  //                   }).get()
+  //   		);
+		$.ajax({
+		            type: "POST",
+		            url: base_url+"/iisp_skills/ajax_settings",
+		            data: datastring,
+		            dataType: "json",
+		            success: function(data) {
+		                //var obj = jQuery.parseJSON(data); if the dataType is not specified as json uncomment this
+		                // do what ever you want with the server response
+		            },
+		            error: function(){
+		                  alert('error handing here');
+		            }
+		        });
 	});
 });
 
--- themes/settings-page.tpl.php
+++ themes/settings-page.tpl.php
@@ -23,7 +23,7 @@
 	
 </div>
 
-<form method="post" id="profile_settings_2">
+<form method="post" id="profile_settings_ga">
 <div class="tabs col-md-12" style="margin-top:30px">
   <ul class="nav nav-tabs">
 	<li class="active"><a href="#tab-1" data-toggle="tab">System Attribute Settings</a></li>
@@ -102,7 +102,7 @@
 											</select>
 											</div>
 										</td>
-										<td><input type="checkbox" name="goal_advanced" id="advance" value="1"></td>
+										<td><input type="checkbox" name="goal_advanced" id="goal_advance" value="1"></td>
 										<td><input type="button" value="Add" class="btn-sm btn-success btn" id="goal-add-button"></td>
 									</tr>
 								</tbody>
@@ -115,8 +115,73 @@
 			</div>
 		</div>
 		<div class="tab-pane fade " id="tab-3">
-			<p>Skills Manager is a tool designed to help organisations understand the skills in their teams and to inform decisions on training, professional development, project management and resource planning.</p><p>The skills profiles created using the Skills Profiler are based upon the IISP Skills Framework.</p><h2>	Skills Manager allows organisations to:</h2><div class="list"><ul>	<li>		<strong>Understand and visualise the skills of their teams</strong> - Asking current employees to create an individual profile can help managers understand what skills their teams already have. This insight can improve succession planning; help you build the right team for a project; and enable you to check if expertise required is available in-house before hiring externally.</li>	<li>		<strong>Make appraisals easier for everyone</strong> - By updating profiles regularly, teams can prepare for appraisals and demonstrate their progress more effectively, while managers can assess each profile against a standard framework.</li>	<li>		<strong>Plan professional development and use training budgets wisely</strong> - When organisations use Skills Manager regularly, potential skills gaps are easier to identify and managers can organise more targeted training. Team members can maintain a record of their skills development and compare with job roles and industry benchmarks.</li>	<li>		<strong>Create new skills profiles</strong> - Compare your teams profiles to profiles you create to see who is the closest match, while outlining the targeted skills for your team.</li></ul></div><p>Skills Manager can be used by organisations of any size.</p><p><a href="http://www.infosecskills.net/?q=contact">Contact us</a> if you wish to use the Skills Manager in your organisation.</p>
+			<p>Make BCS Personal Development Plan even more personal to you by creating your own customised fields within your Development Goals. Give the field a name, choose a type and identify if it is a field you will use regularly or (if not) tick it as an Advanced field to place it into the Advanced fields area.</p>		
+			
+			<div class="panel-group col-md-12" id="accordion">
+				<div class="panel panel-default">
+					<div class="panel-heading">
+						<h4 class="panel-title">
+							<a data-parent="#accordion" href="#accordion-1">
+								Activity Defined Fields
+							</a>
+						</h4>
+					</div>
+					<div id="accordion-1" class="panel-collapse collapse in">
+						<div class="panel-body">
+							<div class="append-activity">
+								<table id="activity-fields"><thead><tr><td>Title</td><td>Type</td><td>Advanced</td><td>Actions</td></tr></thead>
+								<tbody></tbody>
+								</table>
+							</div>
+						</div>
+					</div>
+				</div>
+				
+			</div>
+
+			<div class="panel-group col-md-12" id="accordion" style="margin-top:25px;">
+				<div class="panel panel-default">
+					<div class="panel-heading">
+						<h4 class="panel-title">
+							<a data-parent="#accordion" href="#accordion-1">
+								Add new activity fields
+							</a>
+						</h4>
+					</div>
+					<div id="accordion-1" class="panel-collapse collapse in">
+						<div class="panel-body">
+							<div class="activitys-enter">
+								<table id="activity-fields-enter"><thead><tr><td>Title</td><td>Type</td><td>Advanced</td><td>Actions</td></tr></thead>
+								<tbody>
+									<tr>
+										<td><div class="form-group"><input type="text" name="activity_name" class="form-control" value="" id="activity_name"></div></td>
+										<td>
+											<div class="form-group form-type-select">
+											<select name="activity_type" class="form-select form-control" id="activity_type">
+												<option value="">Select</option>
+												<option value="textfield">Text</option>
+												<option value="int">Numerical</option>
+												<option value="date">Date</option>
+												<option value="decimal">Decimal</option>
+											</select>
+											</div>
+										</td>
+										<td><input type="checkbox" name="activity_advanced" id="activity_advance" value="1"></td>
+										<td><input type="button" value="Add" class="btn-sm btn-success btn" id="activity-add-button"></td>
+									</tr>
+								</tbody>
+								</table>
+							</div>	
+						</div>
+					</div>
+				</div>
+				
+			</div>
 		</div>
 	</div>
+</div>
+
+<div class="save-ajax-form col-md-12 " style="magin-top:10px">
+	<input type="submit" name="save_from" value="SAVE ALL SETTINGS" class="btn btn-default btn-submit-second">
 </div>
 </form>
