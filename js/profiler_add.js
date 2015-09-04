3449d05d25c2b2e9d0e3ad2440dc6b9cde4edba1
unknown <sr.rauniyar@gmail.com>
2015-09-04 20:17:39 +0545

    Share and profile skills completed

--- js/profiler_add.js
+++ js/profiler_add.js
@@ -3,14 +3,26 @@ jQuery(document).ready(function($) {
 	 var pathArray = window.location.pathname.split( '/' );
 	 var secondLevelLocation = pathArray[1];
 	 var base_url = 'http://'+root;
+	 $('.share_div').hide();
 	$('.toggle-span').click(function(){
 		$( ".inner-accordion" ).slideToggle( "slow", function() {});
 	});
 
-	var profile_info_id = $("#profile_info_id").val(); 
+	$('body').on('click', '.share_info', function(){
+		var radio_share_val_yes =$('#share_info_yes_id').val();
+		var radio_share_val_no =$('#share_info_no_id').val();
+		if($("#share_info_yes_id").is(":checked")){
+			$('.share_div').show();
+		}else{
+			$('.share_div').hide();
+		}
+	});
+
+
+	var profile_info_id = $("#profile_info_id").val();
 
 	$.ajax({
-                url: base_url + '/iisp_skills/ajax_skills_profile_data/'+profile_info_id,
+                url: base_url + '/'+ secondLevelLocation +'/iisp_skills/ajax_skills_profile_data/'+profile_info_id,
                 data: {"test":"test"},
                 dataType: 'json',
                 method:'POST',
@@ -20,11 +32,19 @@ jQuery(document).ready(function($) {
                   					var score = val.score;
                   					var skillLevel = val.iisp_skills_id;
                   				   $('#setLevelDiv'+skillLevel + ' .level-set').text('Level ' + score); 
-								   $('#setlevel-btn-value'+skillLevel).text('Set Level ' + score); 
+								   $('#setlevel-btn-value'+skillLevel).text('Level ' + score); 
 								   $('#setlevel-btn-value'+skillLevel).attr("data-checkboxid",score);
 								   $('#checkbox-'+score+'-skill-'+skillLevel).css("background-color", "green");
+								   $('#checkbox-reset-skill-'+skillLevel).removeClass("score-not-set");
+								   $('#checkbox-reset-skill-'+skillLevel).addClass("score-already-set");
 						  });
                   },
+                  error: function() {
+				    //error loading content
+				  },
+				  complete: function() {
+				  	$('.score-not-set').css('background-color','green');
+				  }
               });
 
 	$('#viewing_profile_id').change(function(){
--- themes/profiler-page.tpl.php
+++ themes/profiler-page.tpl.php
@@ -20,6 +20,7 @@
     <div>
   </div>
     <?php foreach($skill_categories as $sc): ?>
+
     <?php $skill = get_skills_by_skill_category($sc->id); ?>
        <div class="panel-group" id="accordion1">
           <div class="panel panel-default">
@@ -41,20 +42,30 @@
                     <div class="panel-heading">
                       <h4 class="panel-title"><a data-toggle="collapse"  data-parent="#accordion2" href="#collapseInner<?php echo $sk->id;?>" class='collapsed'>
                        <?php echo $sk->name; ?>
+                       <?php $scorerange = explode('-', $sk->scorerange);?>
                       </a>
                       <span class="checkbox-wrapper" style="    position: absolute;
                             left: 52%;
                             margin-top: -35px;">
-                      <?php for($i=1; $i<=7; $i++) : ?>
-                      <span class="checkbox-style checkbox-skill<?php echo $sk->id; ?>" id="checkbox-<?php echo $i; ?>-skill-<?php echo $sk->id; ?>" style="cursor:pointer;" onClick="setLabels(<?php echo $i; ?>, <?php echo $sk->id; ?>)" value="<?php  echo $i; ?>"><?php echo $i; ?></span>
+                      <span class="checkbox-style checkbox-reset score-not-set checkbox-skill<?php echo $sk->id; ?>" id="checkbox-reset-skill-<?php echo $sk->id; ?>" style="cursor:pointer;" onClick="resetLabels(<?php echo $sk->id; ?>)" value="<?php echo $sk->id; ?>">-</span>
+                      <?php for($i=1; $i<=6; $i++) : ?>
+                      <?php if ($i >= $scorerange[0] && $i <= $scorerange[1]) { ?>
+                          <span class="checkbox-style checkbox-skill<?php echo $sk->id; ?>" id="checkbox-<?php echo $i; ?>-skill-<?php echo $sk->id; ?>" style="cursor:pointer;" onClick="setLabels(<?php echo $i; ?>, <?php echo $sk->id; ?>)" value="<?php  echo $i; ?>"><?php echo $i; ?></span>
+                      <?php } else { ?>
+                          <span class="checkbox-style" id="checkbox-<?php echo $i; ?>-skill-<?php echo $sk->id; ?>" style="border: 1px dotted #000;background-color:#A7A385" value=""><?php echo $i; ?></span>
+                      <?php } ?>
                       <?php endfor; ?>
+
+                      <?php //for($i=1; $i<=7; $i++) : ?>
+                      <!-- <span class="checkbox-style checkbox-skill<?php echo $sk->id; ?>" id="checkbox-<?php echo $i; ?>-skill-<?php echo $sk->id; ?>" style="cursor:pointer;" onClick="setLabels(<?php echo $i; ?>, <?php echo $sk->id; ?>)" value="<?php  echo $i; ?>"><?php echo $i; ?></span> -->
+                      <?php //endfor; ?>
                       </span>         
                       </h4>
                     </div>
                     <!-- <div id="collapseInnerOne<?php //echo $sklil->id;?>" class="panel-collapse collapse in"> -->
                     <div id="collapseInner<?php echo $sk->id;?>" class="panel-collapse collapse">
                       <div class="panel-body">
-                                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam laoreet sodales lacinia. Curabitur ut purus tincidunt, iaculis elit a, eleifend augue. Phasellus blandit augue vel sollicitudin blandit.
+                              <?php echo $sk->field_desc; ?>    
                       </div>
 
                       <span class="level-text" id="setLevelDiv<?php echo $sk->id; ?>" style="text-align:center; margin-left:48%"> 
@@ -65,7 +76,7 @@
                       <div class="inner-accordion" id="innerDiv<?php echo $sk->id; ?>">
                         <span>
                           <p class="competencies" style="margin-left: 11%;">If you meet any of the competencies for a level you can consider yourself to be at that level.</p>
-                          <p class="setlevelbtn" style="margin-left: 42%"><button  onClick="insertLabels(<?php echo $sk->id; ?>,<?php echo $user_profile_info->id ?>, <?php echo $user->uid; ?>)" class="button-primary btn btn-default" id="setlevel-btn-value<?php echo $sk->id; ?>">Set Level</button><p>
+                          <p class="setlevelbtn" style="margin-left: 42%"><button  onClick="insertLabels(<?php echo $sk->id; ?>,<?php echo $user_profile_info->id ?>, <?php echo $user->uid; ?>)" class="button-primary btn btn-default levelbtn" id="setlevel-btn-value<?php echo $sk->id; ?>">Set Level</button><p>
                           <p data-toggle="collapse" class="collapsed" data-parent="#accordions-sub" style="margin-left:44%"><a href="#" class="toggle-span">Close Tab</a>
                         </span>
                       </div>
@@ -85,7 +96,7 @@
   </div> 
   <div class="col-md-3 left-panel-profile">
     <table> 
-      <tr><td style='border-top:none;'><input type="button" value="Share" class="button-primary btn btn-default profile-left-button" id="share-add-button"></td></tr>
+      <tr><td style='border-top:none;'><input type="button" data-toggle="modal" data-target="#myModal" value="Share" onClick="insertShareLink(<?php echo $user_profile_info->id; ?>,0)" class="button-primary btn btn-default profile-left-button" id="share-add-button"></td></tr>
       <tr><td style='border-top:none;'><input type="button" value="Compare" class="button-primary btn btn-default profile-left-button" id="compare-add-button"></td></tr>
       <tr><td style='border-top:none;'><input type="button" value="Print" class="button-primary btn btn-default profile-left-button" id="print-add-button"></td></tr>
       <tr><td style='border-top:none;'><input type="button" value="SFIA" class="button-primary btn btn-default profile-left-button" id="sifa-add-button"></td></tr>
@@ -93,8 +104,40 @@
     </table>
   </div>
 </div>
-<script type="text/javascript">
 
+<!-- Modal -->
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title" id="myModalLabel">Share <?php  echo ucwords($user_profile_info->name); ?> </h4>
+      </div>
+      <div class="modal-body">
+        <p> Do you want to share this profile with others?</p>
+        <p>
+          <input type="radio" name="radio_share" value="1" id= "share_info_yes_id" class="share_info">Yes
+          <input type="radio" name="radio_share" value="0" id= "share_info_no_id" class="share_info" checked="checked">No
+        </p>
+        <div class="share_div">
+          <p>
+            It’s easy to share your skills profile using the 'Share' option. This creates a unique web link for your personal profile. You can share the link with colleagues, your employer or recruiters via email, LinkedIn or other social media. Click refresh to create a new link and disable previously shared versions of this profile. 
+          </p>
+          <p class="link-to-share"></p>
+          <p class="share_link">
+             <a href="http://localhost/infoseskills.net/?q=iisp_profiler/profiles/preview" class="btn btn-default" target="_blank">Preview</a>
+             <a href="" class="btn btn-default email-sharelink" target="_blank">Email</a>
+             <button type="button" class="btn btn-default ref-sharelink" onClick="insertShareLink(<?php echo $user_profile_info->id; ?>,1)">Refresh</button>
+          </p>
+        </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+<script type="text/javascript">
 
 function setLabels(label, skillLevel){
    jQuery('#setLevelDiv'+skillLevel + ' .level-set').text('Level ' + label); 
@@ -102,19 +145,53 @@ function setLabels(label, skillLevel){
    jQuery('#setlevel-btn-value'+skillLevel).attr("data-checkboxid",label);
 }
 
+function resetLabels(skillLevel){
+   jQuery('#setlevel-btn-value'+skillLevel).text('Reset Level');
+   jQuery('#setlevel-btn-value'+skillLevel).attr("data-checkboxid",0);
+}
+
+function insertShareLink(profileid,refresh_link){
+    jQuery.ajax({
+                  url:'<?php echo $base_url; ?>/iisp_skills/ajax_set_share_link',
+                  data: {"iisp_profiles_id":profileid,"refresh_link":refresh_link},
+                  dataType: 'json',
+                  method:'POST',
+                    success:function (data)
+                    {
+                        jQuery('.link-to-share').text('Share link : ' + data.sharing_links);
+                        jQuery('.email-sharelink').attr("href","mailto:?subject=Share&amp;body="+data.sharing_links);
+                    },
+                });
+}
+
 function insertLabels(skillLevel,profileid,userid){
     var score = jQuery('#setlevel-btn-value'+skillLevel).attr('data-checkboxid');
-   jQuery('.checkbox-skill'+skillLevel).css("background-color", "#CCC");
-   jQuery('#checkbox-'+score+'-skill-'+skillLevel).css("background-color", "green");
-    jQuery.ajax({
-                url:'<?php echo $base_url; ?>/iisp_skills/ajax_skills_profile',
-                data: {"score":score,"iisp_skills_id":skillLevel,"iisp_profiles_id":profileid,"uid":userid},
-                dataType: 'json',
-                method:'POST',
-                  success:function (data)
-                  {
+    if(score != 0){ 
+     jQuery('.checkbox-skill'+skillLevel).css("background-color", "#CCC");
+     jQuery('#checkbox-'+score+'-skill-'+skillLevel).css("background-color", "green");
+      jQuery.ajax({
+                  url:'<?php echo $base_url; ?>/iisp_skills/ajax_skills_profile',
+                  data: {"score":score,"iisp_skills_id":skillLevel,"iisp_profiles_id":profileid,"uid":userid},
+                  dataType: 'json',
+                  method:'POST',
+                    success:function (data)
+                    {
 
-                  },
-              });
+                    },
+                });
+    }else{
+     jQuery('.checkbox-skill'+skillLevel).css("background-color", "#CCC");
+     jQuery('#checkbox-reset-skill-'+skillLevel).css("background-color", "green");
+      jQuery.ajax({
+                  url:'<?php echo $base_url; ?>/iisp_skills/remove_skills_profile',
+                  data: {"iisp_skills_id":skillLevel,"iisp_profiles_id":profileid,"uid":userid},
+                  dataType: 'json',
+                  method:'POST',
+                    success:function (data)
+                    {
+
+                    },
+                });
+    }
 }
 </script>
